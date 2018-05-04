3bc5caf3cb5def731befb711598e1232856bede9
MorganeT <morgane.treuillet@live.fr>
2018-05-04 13:14:40 +0200

    Update TestConnexion.php

--- TestConnexion.php
+++ TestConnexion.php
@@ -5,9 +5,13 @@
   $connecte = $user->identification($_SESSION["user"],$_SESSION["password"]);
 
   // Si non connecté
-  if($connecte != 0)
+  if($connecte[0] != 0)
   {
     // Redirection vers la page de connexion
     header("Location: connexion.php");
+  }
+  else
+  {
+    echo "Vous êtes connecté en tant qu utilisateur d id".$connecte[1];
   }
 ?>
