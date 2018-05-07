54dac7d889516d9f4949c4fafa9960ea4c827c75
sapphaos <apollinegaillar@gmail.com>
2018-05-07 18:12:33 +0200

    début de page d'accueil

--- Interface/home.php
+++ Interface/home.php
@@ -9,7 +9,13 @@
     <body onload="changeColor();">
       <?php include_once 'header.php';?>
       <div id="global">
-        <p>Page d'accueil</p>
+        <h3>Bienvenue!</h3>
+
+        <p>Ce site est un site permettant de jouer au go en ligne. </br>Le go est un jeu de plateau originaire de Chine. Il oppose deux adversaires qui placent à tour de rôle des pierres, respectivement noires et blanches, sur les intersections d'une grille appelée goban.</p>
+
+        <p>Avant de pouvoir jouer, il faudra vous connecter avec un compte valide. </p>
+
+        <p>Vous pouvez consulter les règles détaillées du jeu de go <a href=regles_Go.php>ici</a> et les règles détaillées de notre site <a href=regles_tchat.php>ici</a>.</p>
 
         <footer>
           <p>Projet Développement d'applications web - Université de Bourgogne - Groupe 2</p>
